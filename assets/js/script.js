6daeee5785b6e36740c71ac721d6891a45abb08a
cmikedev <carberrm@gmail.com>
2022-07-23 10:30:28 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function runGame(){}
+
+function checkAnswer() {}
+
+function calculateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrontAnswer() {}
+
+function displayAdditionQuestion() {}
+
+function displaySubtractQuesion() {}
+
+function displayMultiplyQuestion() {}
